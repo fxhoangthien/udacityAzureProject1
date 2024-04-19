e476486d131772f745bbd2509c50b84b148a2307
fxhoangthien@gmail.com <fxhoangthien@gmail.com>
2024-04-19 20:58:22 +0700

    update

---
+++ FlaskWebProject/__init__.py
@@ -0,0 +1,29 @@
+"""
+The flask application package.
+"""
+import logging
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_session import Session
+
+app = Flask(__name__)
+app.config.from_object(Config)
+# TODO: Add any logging levels and handlers with app.logger
+# logging.basicConfig(filename='app.log', level=logging.DEBUG,
+#                     format='%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://thiennh16:122Tranhungdao@thiennh16.database.windows.net/thiennh16?driver=ODBC+Driver+17+for+SQL+Server'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# app.logger.setLevel(logging.WARNING)
+# streamHandler = logging.StreamHandler()
+# streamHandler.setLevel(logging.WARNING)
+# app.logger.addHandler(streamHandler)
+
+Session(app)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+import FlaskWebProject.views
